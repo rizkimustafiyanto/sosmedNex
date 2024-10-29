a278e134c9d312af224d068131f0d5805c897c05
rizkimustafiyanto <rizki.mustafiyanto@gmail.com>
2024-10-29 22:34:07 +0700

    .

--- backend/migrations/20241029142955-add-dummy-posts.js
+++ backend/migrations/20241029142955-add-dummy-posts.js
@@ -4,18 +4,19 @@ const dummyPosts = [
   {
     userId: 1,
     content: "Hello, this is my first post!",
-    imageUrl: "https://example.com/image1.jpg",
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/54/6c/26/546c26bd-38da-2141-e1e4-64c03364a8eb/AppIcon-0-0-1x_U007emarketing-0-6-0-85-220.png/1200x630wa.png",
     createdAt: new Date(),
     updatedAt: new Date(),
   },
   {
     userId: 1,
-    content: "This is another post!",
-    imageUrl: null,
+    content: "IM DEADPOOL!",
+    imageUrl:
+      "https://www.greenscene.co.id/wp-content/uploads/2023/02/dead-3.jpg",
     createdAt: new Date(),
     updatedAt: new Date(),
   },
-  // Tambahkan data dummy lainnya sesuai kebutuhan
 ];
 
 module.exports = {
